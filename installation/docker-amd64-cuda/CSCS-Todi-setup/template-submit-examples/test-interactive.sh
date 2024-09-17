1a6a15965a74e14a107191ae7d8fe29fb60814b5
RiccardoBrioschi <brioschiriccardo.job@gmail.com>
2024-09-17 10:25:43 +0200

    changing template variable names

--- installation/docker-amd64-cuda/CSCS-Todi-setup/template-submit-examples/test-interactive.sh
+++ installation/docker-amd64-cuda/CSCS-Todi-setup/template-submit-examples/test-interactive.sh
@@ -2,12 +2,12 @@
 
 # Enroot + Pyxis
 
-export PROJECT_ROOT_AT=$SCRATCH/template-project-name/dev
+export PROJECT_ROOT_AT=$SCRATCH/deep-alignment/dev
 
 srun \
   -J template-test \
   --pty \
-  --container-image=$CONTAINER_IMAGES/claire+smoalla+template-project-name+amd64-cuda-root-latest.sqsh \
+  --container-image=$CONTAINER_IMAGES/claire+smoalla+deep-alignment+amd64-cuda-root-latest.sqsh \
   --no-container-mount-home \
   --no-container-remap-root \
   --no-container-entrypoint \
---
+++ src/deep_alignment/utils/__init__.py
@@ -0,0 +1,1 @@
+from deep_alignment.utils import seeding
